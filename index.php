73de1b8286e14c0fb359c86343def2b1aa6bc6ce
jpmaster26 <36096933+jpmaster26@users.noreply.github.com>
2018-02-05 18:55:38 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+ <html>
+<head>
+<title>villarin design</title>
+</head>
+<body style="background-color:powderblue;"><center><h1>BIOGRAPHY</h1></center><br>
+<p><front face = 'Georgia'><center>Hi, my name is John Paul B. Villarin, a resident of Tangke 1, City of Naga, Cebu.
+ I belong to a one big happy family of nine and the eldest of three boys. I am a first </center>year student of Cebu 
+Technological University (CTU) studying Bachelor of Science in Information Communication of Technology (BSICT).
+I decided to take this course to learn abaout computers and gain skill that I can use in my future work. After 
+school hours, I spend my free time in playing basketball and online games with my friends.</front></p><br>
+<p><h1><center>My Personal Information</center></h1></p><br>
+<p><front face = 'Georgia'><b>Age:</b>24 years old</p>
+<p><front face = 'Georgia'><b>Sex:</b>Male</p>
+<p><front face = 'Georgia'><b>Status:</b>Single</p>
+<p><front face = 'Georgia'><b>Religion:</b>Roman Catholic</p>
+<p><front face = 'Georgia'><b>Citizenship:</b>Filipino</p>
+<p><front face = 'Georgia'><b>Email Address:</b>jp.master89@yahoo.com</p>
+<p><front face = 'Georgia'><b>Contact no.:</b>09755576159</p><br>
+<p><h1><center>Educational Background</center></h1></p><br>
+<p><front face = 'Georgia'><b>Elementary:</b>Naga Central Elementary School</p>
+<p><front face = 'Georgia'><b>Year Graduate:</b>2005-2006</p>
+<p><front face = 'Georgia'><b>High School:</b>Naga National High School</p>
+<p><front face = 'Georgia'><b>Year Graduate:</b>2010-2011</p>
+</body>
+</html>
